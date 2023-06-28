88f00c4bd0cd0fcd31dfcc29de171faa53dbca4f
HuzaizKhalidQureshi <huzaizqureshi@gmail.com>
2023-06-28 21:16:09 +0500

    created resposniveness in Home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="w-full h-screen bg-gradient-to-b from-black via-black to-gray-800"
+      className=" w-full h-fit bg-gradient-to-b from-black via-black to-gray-800 "
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
-        <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-blue-500 mb-4">
+      <div className=" max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4 ">
+        <div className=" flex flex-col justify-center h-full mt-24 mb-10">
+          <h2 className=" text-4xl sm:text-7xl   font-bold text-blue-500 mt-50">
             I'm a
             <br />
             <span className="block text-red-500">
